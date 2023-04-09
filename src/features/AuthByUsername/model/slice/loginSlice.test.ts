b6e07cc77f5ba964fb42ed75095a0b2be5f76982
Sasha <LoveCoddingALot>
2023-04-09 14:59:09 +0300

    add tests for login module

--- src/features/AuthByUsername/model/slice/loginSlice.test.ts
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -9,7 +9,7 @@ describe('loginSlice.test', () => {
         expect(loginReducer(
             state as LoginSchema,
             loginActions.setUsername('123123')
-        )).toBe({username: '123123'})
+        )).toEqual({username: '123123'})
     })
 
     test('test set password', () => {
@@ -17,6 +17,6 @@ describe('loginSlice.test', () => {
         expect(loginReducer(
             state as LoginSchema,
             loginActions.setPassword('123123')
-        )).toBe({password: '123123'})
+        )).toEqual({password: '123123'})
     })
 })
