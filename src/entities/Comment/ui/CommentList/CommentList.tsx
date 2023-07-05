096190f4cb8094e89b19e8de0ea0867b2beb8edf
Sasha <LoveCoddingALot>
2023-07-05 23:25:30 +0300

    add comment list

---
+++ src/entities/Comment/ui/CommentList/CommentList.tsx
@@ -0,0 +1,28 @@
+import { classNames } from 'shared/lib/classNames/classNames'
+import cls from './CommentList.module.scss'
+import { memo } from 'react'
+import { Comment } from '../../model/types/comment'
+import { Text } from 'shared/ui/Text/Text'
+import { CommentCard } from '../CommentCard/CommentCard'
+
+interface CommentListProps {
+    className?: string;
+    comments?: Comment[];
+    isLoading?: boolean;
+}
+
+export const CommentList = memo((props: CommentListProps) => {
+    const {
+        className,
+        comments,
+        isLoading
+    } = props
+    return (
+        <div className={classNames(cls.CommentList, {}, [className])}>
+            {comments?.length
+                ? comments.map(comment => <CommentCard key={comment.id} isLoading={isLoading} className={cls.comment} comment={comment} />)
+                : <Text text={"Комментарии отсутствуют"} />
+            }
+        </div>
+    )
+})
